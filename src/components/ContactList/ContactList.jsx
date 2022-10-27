9892bcf12ad305f8f4d2d8d91df177c192e12f0f
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-10-27 23:19:29 +0300

    Add propTypes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
@@ -9,6 +10,15 @@ const ContactList = ({ contacts, onDeleteContact }) => {
       </li>
       )}</ul>
   );
+}
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  })),
+  onDeleteContact: PropTypes.func.isRequired,
 }
 
 export default ContactList;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,16 @@
-const Filter = ({value, onChange}) => {
+import PropTypes from 'prop-types';
+
+const Filter = ({ value, onChange }) => {
   return (
     <label htmlFor="">Find contacts by name
       <input type="text" value={value} onChange={onChange} />
     </label>
   )
+}
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 }
 
 export default Filter;
